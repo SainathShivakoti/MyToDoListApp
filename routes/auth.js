67e4619b16bd4da149d6c2e7ee6a17c19b12e67a
SainathShivakoti <sainath.shivakoti@gmail.com>
2024-11-13 00:09:57 +0530

    Increased minimum character length needed for password

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.post('/register', [
   check('username', 'Username is required').not().isEmpty(),
   check('email', 'Please include a valid email').isEmail(),
-  check('password', 'Password must be 6 or more characters').isLength({ min: 6 })
+  check('password', 'Password must be 8 or more characters').isLength({ min: 8 })
 ], async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
